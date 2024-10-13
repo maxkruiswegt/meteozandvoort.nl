94dfeb485fa03f4ae667cc70cf5749a8734034d4
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2024-10-13 13:03:24 +0200

    Separate handleIntersection function from observeElements

--- src/utils/observerUtils.js
+++ src/utils/observerUtils.js
@@ -1,3 +1,11 @@
+function handleIntersection(entry, observer) {
+  if (entry.isIntersecting) {
+    entry.target.classList.add('show-element');
+    entry.target.classList.remove('hidden-element');
+    observer.unobserve(entry.target);
+  }
+}
+
 const observeElements = () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -9,14 +17,6 @@ const observeElements = () => {
   hiddenElements.forEach((element) => {
     observer.observe(element);
   });
-
-  function handleIntersection(entry, observer) {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show-element');
-      entry.target.classList.remove('hidden-element');
-      observer.unobserve(entry.target);
-    }
-  }
 };
 
 export { observeElements };
