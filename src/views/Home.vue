dc2bc9ec67ae1877066a36063cd090e4cd5557aa
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2024-10-26 20:35:49 +0200

    Reformat Home.vue an fix horizontal alignment of API error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,13 @@ import { ref, onMounted } from 'vue';
 import { useWeatherStore } from '@/stores/WeatherStore';
 import WeatherComponent from '@/components/weather/WeatherComponent.vue';
 import WindComponent from '@/components/weather/WindComponent.vue';
-import { convertFahrenheitToCelsius, convertInchesOfMercuryToMillibar, convertMphToKmh, convertMphToWindScale, convertWindDirection } from '@/utils/weatherUtils';
+import {
+  convertFahrenheitToCelsius,
+  convertInchesOfMercuryToMillibar,
+  convertMphToKmh,
+  convertMphToWindScale,
+  convertWindDirection,
+} from '@/utils/weatherUtils';
 
 const weatherStore = useWeatherStore();
 
@@ -23,14 +29,68 @@ onMounted(async () => {
 
 const columns = window.innerWidth > 768 ? 4 : 2;
 const weatherComponents = [
-  { title: 'Temperatuur', value: () => convertFahrenheitToCelsius(weatherStore.temperature).toFixed(1), unit: '°C', icon: 'device_thermostat', timespan: 'nu' },
-  { title: 'Gevoelstemp.', value: () => convertFahrenheitToCelsius(weatherStore.thwIndex).toFixed(1), unit: '°C', icon: 'heat', timespan: 'nu' },
-  { title: 'Regenintensiteit', value: () => weatherStore.rainRateLast.toFixed(1), unit: 'mm/u', icon: 'umbrella', timespan: 'nu' },
-  { title: 'Regenval', value: () => weatherStore.rainfallToday.toFixed(1), unit: 'mm', icon: 'rainy', timespan: 'vandaag' },
-  { title: 'Luchtvochtigheid', value: () => weatherStore.humidity, unit: '%', icon: 'water_drop', timespan: 'nu' },
-  { title: 'Dauwpunt', value: () => convertFahrenheitToCelsius(weatherStore.dewPoint).toFixed(1), unit: '°C', icon: 'dew_point', timespan: 'nu' },
-  { title: 'Luchtdruk', value: () => convertInchesOfMercuryToMillibar(weatherStore.barometricPressure).toFixed(1), unit: 'mb', icon: 'compress', timespan: 'nu' },
-  { title: 'Luchtdruk Trend', value: () => convertInchesOfMercuryToMillibar(weatherStore.barometricTrend).toFixed(1), unit: 'mb', icon: () => weatherStore.barometricTrend > 0 ? 'trending_up' : 'trending_down', timespan: '3 uur' },
+  {
+    title: 'Temperatuur',
+    value: () =>
+      convertFahrenheitToCelsius(weatherStore.temperature).toFixed(1),
+    unit: '°C',
+    icon: 'device_thermostat',
+    timespan: 'nu',
+  },
+  {
+    title: 'Gevoelstemp.',
+    value: () => convertFahrenheitToCelsius(weatherStore.thwIndex).toFixed(1),
+    unit: '°C',
+    icon: 'heat',
+    timespan: 'nu',
+  },
+  {
+    title: 'Regenintensiteit',
+    value: () => weatherStore.rainRateLast.toFixed(1),
+    unit: 'mm/u',
+    icon: 'umbrella',
+    timespan: 'nu',
+  },
+  {
+    title: 'Regenval',
+    value: () => weatherStore.rainfallToday.toFixed(1),
+    unit: 'mm',
+    icon: 'rainy',
+    timespan: 'vandaag',
+  },
+  {
+    title: 'Luchtvochtigheid',
+    value: () => weatherStore.humidity,
+    unit: '%',
+    icon: 'water_drop',
+    timespan: 'nu',
+  },
+  {
+    title: 'Dauwpunt',
+    value: () => convertFahrenheitToCelsius(weatherStore.dewPoint).toFixed(1),
+    unit: '°C',
+    icon: 'dew_point',
+    timespan: 'nu',
+  },
+  {
+    title: 'Luchtdruk',
+    value: () =>
+      convertInchesOfMercuryToMillibar(weatherStore.barometricPressure).toFixed(
+        1
+      ),
+    unit: 'mb',
+    icon: 'compress',
+    timespan: 'nu',
+  },
+  {
+    title: 'Luchtdruk Trend',
+    value: () =>
+      convertInchesOfMercuryToMillibar(weatherStore.barometricTrend).toFixed(1),
+    unit: 'mb',
+    icon: () =>
+      weatherStore.barometricTrend > 0 ? 'trending_up' : 'trending_down',
+    timespan: '3 uur',
+  },
 ];
 </script>
 
@@ -41,64 +101,101 @@ const weatherComponents = [
       <p>Welkom op meteozandvoort.nl</p>
     </div>
     <div class="update">
-      <div class="refresh" @click="refreshData" :class="{ 'rotate-once': refresh }">
+      <div
+        class="refresh"
+        @click="refreshData"
+        :class="{ 'rotate-once': refresh }"
+      >
         <span class="material-symbols-outlined">refresh</span>
       </div>
-      <small>Laatste update: {{ new Date(weatherStore.lastUpdated).toLocaleString([], {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit'
-      }) }}</small>
+      <small
+        >Laatste update:
+        {{
+          new Date(weatherStore.lastUpdated).toLocaleString([], {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          })
+        }}</small
+      >
     </div>
     <div v-if="weatherStore.isLoading" class="loading">
       <span class="material-symbols-outlined">hourglass_empty</span>
       <p>Gegevens laden...</p>
     </div>
-    <div class="weather" v-else-if="weatherStore.currentWeatherData && weatherStore.historicWeatherData">
-      <WindComponent class="hidden-element" :windNow="{
-        force: convertMphToWindScale(weatherStore.windSpeedLast),
-        speed: convertMphToKmh(weatherStore.windSpeedLast).toFixed(1),
-        direction: {
-          name: convertWindDirection(weatherStore.windDirectionLast),
-          degrees: weatherStore.windDirectionLast
-        }
-      }" :windAverage10m="{
-        force: convertMphToWindScale(weatherStore.windSpeedAvgLast10Min),
-        speed: convertMphToKmh(weatherStore.windSpeedAvgLast10Min).toFixed(1),
-        direction: {
-          name: convertWindDirection(weatherStore.windDirectionAvgLast10Min),
-          degrees: weatherStore.windDirectionAvgLast10Min
-        }
-      }" :windMax10m="{
-        force: convertMphToWindScale(weatherStore.windSpeedHiLast10Min),
-        speed: convertMphToKmh(weatherStore.windSpeedHiLast10Min).toFixed(1),
-        direction: {
-          name: convertWindDirection(weatherStore.windDirectionHiLast10Min),
-          degrees: weatherStore.windDirectionHiLast10Min
-        }
-      }" :windAverage24h="{
-        force: convertMphToWindScale(weatherStore.windSpeedAvgLast24Hours),
-        speed: convertMphToKmh(weatherStore.windSpeedAvgLast24Hours).toFixed(1),
-        direction: {
-          name: convertWindDirection(weatherStore.windDirectionAvgLast24Hours),
-          degrees: weatherStore.windDirectionAvgLast24Hours
-        }
-      }" :windMax24h="{
-        force: convertMphToWindScale(weatherStore.windSpeedHiLast24Hours),
-        speed: convertMphToKmh(weatherStore.windSpeedHiLast24Hours).toFixed(1),
-        direction: {
-          name: convertWindDirection(weatherStore.windDirectionHiLast24Hours),
-          degrees: weatherStore.windDirectionHiLast24Hours
-        }
-      }" />
+    <div
+      class="weather"
+      v-else-if="
+        weatherStore.currentWeatherData && weatherStore.historicWeatherData
+      "
+    >
+      <WindComponent
+        class="hidden-element"
+        :windNow="{
+          force: convertMphToWindScale(weatherStore.windSpeedLast),
+          speed: convertMphToKmh(weatherStore.windSpeedLast).toFixed(1),
+          direction: {
+            name: convertWindDirection(weatherStore.windDirectionLast),
+            degrees: weatherStore.windDirectionLast,
+          },
+        }"
+        :windAverage10m="{
+          force: convertMphToWindScale(weatherStore.windSpeedAvgLast10Min),
+          speed: convertMphToKmh(weatherStore.windSpeedAvgLast10Min).toFixed(1),
+          direction: {
+            name: convertWindDirection(weatherStore.windDirectionAvgLast10Min),
+            degrees: weatherStore.windDirectionAvgLast10Min,
+          },
+        }"
+        :windMax10m="{
+          force: convertMphToWindScale(weatherStore.windSpeedHiLast10Min),
+          speed: convertMphToKmh(weatherStore.windSpeedHiLast10Min).toFixed(1),
+          direction: {
+            name: convertWindDirection(weatherStore.windDirectionHiLast10Min),
+            degrees: weatherStore.windDirectionHiLast10Min,
+          },
+        }"
+        :windAverage24h="{
+          force: convertMphToWindScale(weatherStore.windSpeedAvgLast24Hours),
+          speed: convertMphToKmh(weatherStore.windSpeedAvgLast24Hours).toFixed(
+            1
+          ),
+          direction: {
+            name: convertWindDirection(
+              weatherStore.windDirectionAvgLast24Hours
+            ),
+            degrees: weatherStore.windDirectionAvgLast24Hours,
+          },
+        }"
+        :windMax24h="{
+          force: convertMphToWindScale(weatherStore.windSpeedHiLast24Hours),
+          speed: convertMphToKmh(weatherStore.windSpeedHiLast24Hours).toFixed(
+            1
+          ),
+          direction: {
+            name: convertWindDirection(weatherStore.windDirectionHiLast24Hours),
+            degrees: weatherStore.windDirectionHiLast24Hours,
+          },
+        }"
+      />
       <div class="weather-components">
-        <WeatherComponent v-for="(component, index) in weatherComponents" :key="component.title"
-          :title="component.title" :value="component.value()" :unit="component.unit"
-          :icon="typeof component.icon === 'function' ? component.icon() : component.icon"
-          :timespan="component.timespan" :style="{ transitionDelay: `${(index % columns) * 0.2}s` }"
-          class="hidden-element" />
+        <WeatherComponent
+          v-for="(component, index) in weatherComponents"
+          :key="component.title"
+          :title="component.title"
+          :value="component.value()"
+          :unit="component.unit"
+          :icon="
+            typeof component.icon === 'function'
+              ? component.icon()
+              : component.icon
+          "
+          :timespan="component.timespan"
+          :style="{ transitionDelay: `${(index % columns) * 0.2}s` }"
+          class="hidden-element"
+        />
       </div>
     </div>
     <div class="error" v-else>
@@ -110,14 +207,23 @@ const weatherComponents = [
         <span class="material-symbols-outlined">info</span>
         <h3>Over Meteo Zandvoort</h3>
       </div>
-      <p>Deze weergegevens worden rechtstreeks geleverd door het weerstation van <strong>Herman Kruiswegt</strong> in
-        Zandvoort. Herman
-        heeft jarenlang ervaring in meteorologie en biedt nauwkeurige en actuele weerinformatie voor de regio.</p>
-      <p>Naast zijn passie voor meteorologie, biedt Herman ook boekhoudingsdiensten aan via zijn website: <a
-          href="https://decib.nl" target="_blank">decib.nl</a>.</p>
+      <p>
+        Deze weergegevens worden rechtstreeks geleverd door het weerstation van
+        <strong>Herman Kruiswegt</strong> in Zandvoort. Herman heeft jarenlang
+        ervaring in meteorologie en biedt nauwkeurige en actuele weerinformatie
+        voor de regio.
+      </p>
+      <p>
+        Naast zijn passie voor meteorologie, biedt Herman ook
+        boekhoudingsdiensten aan via zijn website:
+        <a href="https://decib.nl" target="_blank">decib.nl</a>.
+      </p>
     </div>
     <footer>
-      <p>Gemaakt met ❤️ door <a href="https://maxkruiswegt.com" target="_blank">Max Kruiswegt</a></p>
+      <p>
+        Gemaakt met ❤️ door
+        <a href="https://maxkruiswegt.com" target="_blank">Max Kruiswegt</a>
+      </p>
     </footer>
   </div>
 </template>
@@ -170,6 +276,7 @@ const weatherComponents = [
 .error {
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 0.25rem;
   margin: 1rem 0;
   color: #cc0000;
@@ -244,14 +351,18 @@ footer {
 }
 
 /* Large Devices */
-@media screen and (min-width: 992px) {}
+@media screen and (min-width: 992px) {
+}
 
 /* Extra Large Devices */
-@media screen and (min-width: 1200px) {}
+@media screen and (min-width: 1200px) {
+}
 
 /* Extra Extra Large Devices */
-@media screen and (min-width: 1600px) {}
+@media screen and (min-width: 1600px) {
+}
 
 /* Very Large Devices */
-@media screen and (min-width: 2560px) {}
+@media screen and (min-width: 2560px) {
+}
 </style>
